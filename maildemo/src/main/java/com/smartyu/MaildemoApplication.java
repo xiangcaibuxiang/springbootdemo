6e3d944064e3331bfeeb101c01de4bdb055684cc
daimabuhuifei <3081760695@qq.com>
2020-08-24 17:20:25 +0800

    Initial commit

---
+++ maildemo/src/main/java/com/smartyu/MaildemoApplication.java
@@ -0,0 +1,13 @@
+package com.smartyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaildemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaildemoApplication.class, args);
+    }
+
+}
---
+++ maildemo/src/test/java/com/smartyu/MaildemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaildemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
